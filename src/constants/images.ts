071db81aadd20d23b1cf00ab2b4fc46d42466485
Maxim Khotsevich <mkhotsevich@gmail.com>
2022-02-22 19:41:41 +0300

    поправил ссылку для бека

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const getImages = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/successes')
+  const { data } = await axios.get('/api/successes')
   return data.data.map((s: { attachment: string }) => s.attachment)
 }
